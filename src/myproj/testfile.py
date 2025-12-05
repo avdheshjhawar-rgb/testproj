853a12e4b4fb8a8a8a07781e4aa5d52383ff97c4
Eugen Ajechiloae <eugenajechiloae@gmail.com>
2025-12-05 17:14:42 +0200

    Update testfile.py

--- src/myproj/testfile.py
+++ src/myproj/testfile.py
@@ -2,7 +2,7 @@ import argparse
 from clearml import Task
 
 if __name__ == "__main__":
-    task = Task.init(project_name="testproj", task_name="testfile")
+    task = Task.init(project_name="testproj", task_name="testfile", reuse_last_task_id=False)
     p = argparse.ArgumentParser()
     p.add_argument("config", type=argparse.FileType("r"))
     args = p.parse_args()
