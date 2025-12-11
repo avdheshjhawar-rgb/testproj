73d1e8bf29b2abe9afa8371e6ea12e58f44dda33
avdhesh <avdhesh.jhawar@clearml.ai>
2025-12-11 16:08:38 +0530

    test-the-changes

--- src/myproj/testfile.py
+++ src/myproj/testfile.py
@@ -3,9 +3,9 @@ from clearml import Task
 
 if __name__ == "__main__":
     print("are we local???", Task.running_locally())
-    task = Task.init(project_name="testproj", task_name="testfile", reuse_last_task_id=False)
-    p = argparse.ArgumentParser()
-    p.add_argument("config", type=argparse.FileType("r"))
-    args = p.parse_args()
-    print("got config", args.config)
-    task.execute_remotely(queue_name="Eugene")
+    task = Task.init(project_name="Project0061", task_name="testfile-git-repo", reuse_last_task_id=False)
+    # p = argparse.ArgumentParser()
+    # p.add_argument("config", type=argparse.FileType("r"))
+    # args = p.parse_args()
+    # print("got config", args.config)
+    task.execute_remotely(queue_name="avdhesh")
